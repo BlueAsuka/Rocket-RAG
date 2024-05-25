bce908dfcf8cd4ba0e1a1511ea9e45ca321e0881
BlueAsuka <denghaoxuan1@126.com>
2024-05-25 20:48:12 +0100

    Update multi_queries_gen_prompt.py

--- rocket_rag/prompts/multi_queries_gen_prompt.py
+++ rocket_rag/prompts/multi_queries_gen_prompt.py
@@ -10,17 +10,20 @@ Lack of Lubrication: In this state, the actuator operates with insufficient lubr
 Backlash: Represents a state where there is a gap or looseness in the connection between the parts of the actuator, leading to inaccuracies and reduced precision in movement.
 You will be offered a report of fault diagnosis and you should extract the fault type from this report.
 EXAMPLE:
-Retrieval results: ['lackLubrication1_40_10_2', 'lackLubrication1_40_7_4', 'lackLubrication1_40_2_3', 'lackLubrication1_40_1_1', 'lackLubrication1_40_2_5']
-Diagonsis results:
-Refined fault type1: Fault in lack of lubrication
-Inference evidence: [lackLubrication1_40_10_2 with <high>, lackLubrication1_40_7_4 with <medium>, lackLubrication1_40_2_3 with <medium>, lackLubrication1_40_1_1 with <low>, lackLubrication1_40_2_5 with <medium>]
-Description of the Fault: The actuator is experiencing a fault in lack of lubrication. This condition can lead to increased friction and potential for wear and tear, impacting performance and longevity. It's important to address this issue promptly to prevent further damage and ensure optimal operation of the system.
+fault_type: lackLubrication
+fault_Description: The actuator is experiencing a fault in lack of lubrication. This condition can lead to increased friction and potential for wear and tear, impacting performance and longevity. It's important to address this issue promptly to prevent further damage and ensure optimal operation of the system.
 
-You should recognize the fault type 'Fault in lack of lubrication' from this report result. Then you can parse the query string for searching.
+You should recognize the fault type 'lackLubrication' from this statment. Then you can parse the query string for searching related to lack of lubrication on linear acuator.
 
-Then you should generate multiple search queries related to how to recover or conduct maintenance for the detected fault type on the linear actuators?
-REMEMBER all generated queries should related to linear actuators fault recovery and maintenance.
+Then you should generate multiple search queries related to how to recover or conduct maintenance for the detected fault type on the linear actuators.
+REMEMBER all generated queries should related to linear actuators fault repair, recovery and maintenance.
+DO NOT add any bullet point or symbols to the query string such as '-', '"', '.' and indexing numbers like 1. 2. 3.
+DO NOT add any introduction words or summarizations to the query string.
+ONLY give me the generated string in your response.
 """
 
-user_prompt = "The fault diagnosis is {res}, \
-please generate {num} multiple search queries WITHOUT bullet point and indexing."
+user_prompt = """
+USER_PROMPT:
+The fault type is {fault_type}.
+The fault description is {fault_description}.
+please generate {num} multiple search queries"""
