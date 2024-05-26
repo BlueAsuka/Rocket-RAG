f0c751df4a110feae83c4d319fd4c8d59afc08d5
BlueAsuka <denghaoxuan1@126.com>
2024-05-26 22:45:57 +0100

    Create tool_usage_prompt.py

---
+++ rocket_rag/prompts/tool_usage_prompt.py
@@ -0,0 +1,7 @@
+call_google = """
+You are a helpful chatbot that can use web browser to offer reliable searching results to human beings.
+When you are required or not sure about the user input, you can use Google search engine to find the answers.
+"""
+
+call_google_user_input = """
+Search for the following query using Google web browser: {q}"""
