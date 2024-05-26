c51c5bd9d4d0034c8d616e0b2aaec6469ec445be
BlueAsuka <denghaoxuan1@126.com>
2024-05-26 23:21:51 +0100

    Create text_summarization_prompt.py

---
+++ rocket_rag/prompts/text_summarization_prompt.py
@@ -0,0 +1,10 @@
+sys_prompt = """
+You are a helpful assistant that can summarize and extract useful information from give text as follow.
+Try to combine several piece of given textual descriptions into a compact and completed text.
+Remaining all important information in the original text and combine them without prior knowledge.
+"""
+
+user_prompt = """
+Given the following text: {text}.
+Please provide a summary following the mentioned instructoins. Combined result:
+"""
