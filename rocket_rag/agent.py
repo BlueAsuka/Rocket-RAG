427de323ef366a1492beaec8d29a8db03b016a7e
BlueAsuka <denghaoxuan1@126.com>
2024-09-05 14:33:13 +0100

    Update agent.py

--- rocket_rag/agent.py
+++ rocket_rag/agent.py
@@ -9,7 +9,6 @@ from openai import OpenAI, AsyncClient, OpenAIError
 from typing import List, Dict
 from tqdm.auto import tqdm
 
-from .utils import fit_transform
 from .node_indexing import NodeIndexer
 from .vector_store import VectorStore
 from .tools import Tools
